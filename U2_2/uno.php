888d57dbb5c3fef1a4b03c699e52203f981b0b07
Piterxus NetBeans <piterxus@gmail.com>
2021-11-02 09:13:23 +0100

    Avanzo ejercicios

---
+++ U2_2/uno.php
@@ -0,0 +1,33 @@
+<!-- Transforma el siguiente bucle for en un bucle while:
+for ($i=5; $i<15; $i++) {
+echo $i."<br>";
+} -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$i=0;
+
+while($i<15){
+    $i++;
+
+    echo $i."<br>";
+   
+
+
+
+}
+
+
+
+
+?>
+</body>
+</html>
